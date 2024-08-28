b92828b887cb2e77a4ae8dfdd190ec43192e45e8
mazharulislam-12 <505mazharulislam@gmail.com>
2024-08-29 03:17:41 +0600

    add to cart

--- src/pages/Collaction.jsx
+++ src/pages/Collaction.jsx
@@ -142,7 +142,7 @@ const Collaction = () => {
                 </div>
 
                 {/* map product */}
-                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-6">
+                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6">
                     {
                         filterProducts.map((item, index) => (
                             <ProductItem key={index} name={item.name} id={item._id} price={item.price} image={item.image} />
