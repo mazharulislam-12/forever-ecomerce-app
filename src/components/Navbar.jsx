19b9cacdf2b8a6965dc98d7e8890b4094ca70313
mazharulislam-12 <505mazharulislam@gmail.com>
2024-08-30 00:41:14 +0600

    login page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,12 +8,12 @@ const Navbar = () => {
 
     const [visable, setVisable] = useState(false);
 
-    const {setShowSearch, getCartCount} = useContext(ShopContext);
+    const { setShowSearch, getCartCount } = useContext(ShopContext);
 
     return (
         <div className='flex  items-center justify-between py-5 font-medium '>
             <Link to='/'>
-                <img  className="w-36" src={assets.logo} alt="logo" />
+                <img className="w-36" src={assets.logo} alt="logo" />
             </Link>
             <ul className="hidden sm:flex gap-5 text-sm text-gray-700 ">
 
@@ -39,7 +39,9 @@ const Navbar = () => {
             <div className="flex items-center gap-6 ">
                 <img onClick={() => setShowSearch(true)} src={assets.search_icon} className="w-5 cursor-pointer" alt="search" />
                 <div className="group relative">
-                    <img className="w-5 cursor-pointer" src={assets.profile_icon} alt="profile-icon" />
+                    <Link to='/login'>
+                        <img className="w-5 cursor-pointer" src={assets.profile_icon} alt="profile-icon" />
+                    </Link>
                     <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4 ">
                         <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded ">
                             <p className="cursor-pointer hover:text-black">My profile</p>
