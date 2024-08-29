4ccad812a44dc2d366989d429fdd80ca681439e5
mazharulislam-12 <505mazharulislam@gmail.com>
2024-08-30 00:01:00 +0600

    order page

--- src/components/CartTotal.jsx
+++ src/components/CartTotal.jsx
@@ -3,7 +3,7 @@ import { ShopContext } from "../context/ShopContext";
 import Title from "./Title";
 
 const CartTotal = () => {
-    const { currency, delivary_fee, getCartAmount } = useContext(ShopContext);
+    const { currency, delivery_fee, getCartAmount } = useContext(ShopContext);
     
     const cartAmount = getCartAmount();
     const subtotal = cartAmount !== undefined && cartAmount !== null ? cartAmount : 0;
@@ -16,17 +16,17 @@ const CartTotal = () => {
             <div className="flex flex-col gap-2 mt-2 text-sm">
                 <div className="flex justify-between">
                     <p>Subtotal</p>
-                    <p>{currency} {subtotal.toFixed(2)}</p> 
+                    <p>{currency} {subtotal.toFixed(2)}</p> {/* দুই দশমিকের জন্য ফর্ম্যাটিং */}
                 </div>
                 <hr />
                 <div className="flex justify-between">
                     <p>Shipping Fee</p>
-                    <p>{currency} {delivary_fee.toFixed(2)}</p>
+                    <p>{currency} {delivery_fee.toFixed(2)}</p> {/* দুই দশমিকের জন্য ফর্ম্যাটিং */}
                 </div>
                 <hr />
                 <div className="flex justify-between">
                     <b>Total</b>
-                    <b>{currency} {(subtotal + delivary_fee).toFixed(2)}</b> 
+                    <b>{currency} {(subtotal + delivery_fee).toFixed(2)}</b> {/* সঠিক টোটাল নিশ্চিত */}
                 </div>
             </div>
         </div>
--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,49 +1,53 @@
 import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 export const ShopContext = createContext();
 
-const ShopContextProdiver = (props) =>{
-
+const ShopContextProdiver = (props) => {
     const currency = '$';
-    const delivary_fee = 10;
-    const [search, setSearch] = useState('')
+    const delivery_fee = 10;  
+    const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
-    const [cartItems, setCartItems] = useState({})
+    const navigate = useNavigate();
 
 
-    const addToCart = async (itemId, size) =>{
+    
+    const [cartItems, setCartItems] = useState(() => {
+        const savedCartItems = localStorage.getItem('cartItems');
+        return savedCartItems ? JSON.parse(savedCartItems) : {}; 
+    });
 
+    
+    useEffect(() => {
+        localStorage.setItem('cartItems', JSON.stringify(cartItems));
+    }, [cartItems]);
+
+    const addToCart = async (itemId, size) => {
         if (!size) {
-            toast.error('Please Select Product Size')
+            toast.error('Please Select Product Size');
             return;
         }
 
         let cartData = structuredClone(cartItems);
         if (cartData[itemId]) {
-            if (cartData[itemId] [size]) {
-                cartData[itemId] [size] +=1
-
-            }
-            else{
+            if (cartData[itemId][size]) {
+                cartData[itemId][size] += 1;
+            } else {
                 cartData[itemId][size] = 1;
-
             }
-        }
-        else{
+        } else {
             cartData[itemId] = {};
-            cartData[itemId] [size] = 1;
-
+            cartData[itemId][size] = 1;
         }
         setCartItems(cartData);
-    }
+    };
 
-
-    const getCartCount = () =>{
+    const getCartCount = () => {
         let totalCount = 0;
-        for(const items in cartItems){
-            for(const item in cartItems[items]){
+        for (const items in cartItems) {
+            for (const item in cartItems[items]) {
                 try {
                     if (cartItems[items][item] > 0) {
                         totalCount += cartItems[items][item];
@@ -54,24 +58,19 @@ const ShopContextProdiver = (props) =>{
             }
         }
         return totalCount;
-
     };
 
-    const updateQuantity = async (itemId,size, quantity) =>{
-        let cartData = structuredClone(cartItems)
-
+    const updateQuantity = async (itemId, size, quantity) => {
+        let cartData = structuredClone(cartItems);
         cartData[itemId][size] = quantity;
-
-        setCartItems(cartData)
-
+        setCartItems(cartData);
     };
 
     const getCartAmount = () => {
         let totalAmount = 0;
         for (const itemId in cartItems) {
-           
             let itemInfo = products.find(product => product._id === itemId);
-            if (itemInfo) {
+            if (itemInfo && itemInfo.price) {  
                 for (const key in cartItems[itemId]) {
                     try {
                         if (cartItems[itemId][key] > 0) {
@@ -84,28 +83,26 @@ const ShopContextProdiver = (props) =>{
             }
         }
         return totalAmount;
-    }
-    
-
-    
-
+    };
 
     const value = {
-        products, currency, delivary_fee,
-        search, setSearch, showSearch, setShowSearch,
-        cartItems, addToCart, 
+        products,
+        currency,
+        delivery_fee,  
+        search, setSearch, 
+        showSearch, setShowSearch,
+        cartItems, addToCart,
         getCartCount,
         updateQuantity,
-        getCartAmount
-
-    }
+        getCartAmount,
+        navigate
+    };
 
     return (
         <ShopContext.Provider value={value}>
             {props.children}
         </ShopContext.Provider>
-    )
-}
-
+    );
+};
 
 export default ShopContextProdiver;
